3ec785beca76d41422062fb6a8ba1cd2229f93ef
Chris Hunt <chris.hunt@hudl.com>
2021-12-04 10:43:35 +0000

    Tidy up day 4

--- 4.py
+++ 4.py
@@ -3,24 +3,19 @@ import numpy as np
 
 def check_row_or_col(card):
     # Rows
-    for i in range(5):
-        if np.sum(card[i]) == 5:
-            return True
+    complete_rows = np.any(np.equal(5, (np.sum(card, axis=1))))
+    if complete_rows:
+        return True
     # Columns
-    for i in range(5):
-        if np.sum(card[j][i] for j in range(5)) == 5:
-            return True
+    complete_columns = np.any(np.equal(5, (np.sum(card, axis=0))))
+    return complete_columns
 
 
 def play_bingo(cards, numbers, last_card=False):
     complete_cards = np.zeros(len(cards))
-    masks = np.zeros((len(cards), 5, 5), dtype=int)
+    masks = np.zeros((len(cards), 5, 5), dtype=bool)
     for number in numbers:
-        for i in range(len(cards)):
-            for j in range(5):
-                for k in range(5):
-                    if cards[i][j][k] == number:
-                        masks[i][j][k] = 1
+        masks += np.equal(number, cards)
 
         for i in range(len(cards)):
             if check_row_or_col(masks[i]):
@@ -29,19 +24,34 @@ def play_bingo(cards, numbers, last_card=False):
                 else:
                     complete_cards[i] = 1
                     if np.sum(complete_cards) == len(cards):
-                        print(complete_cards)
                         return cards[i], masks[i], number
 
     return None, None, None
 
 
-def get_sum(card, mask, marked=1):
-    sum = 0
-    for i in range(5):
-        for j in range(5):
-            if mask[i][j] == marked:
-                sum += card[i][j]
-    return sum
+def get_sum(card, mask):
+    return np.sum(card * np.invert(mask))
+
+
+def generate_cards(lines):
+    numbers = []
+    cards = []
+    card = []
+    for i, line in enumerate(lines):
+        if i == 0:
+            numbers = [int(x) for x in line.split(",")]
+            continue
+
+        if line == "":
+            if i > 1:
+                cards.append(card)
+                card = []
+            continue
+
+        card.append(np.array([int(line[3 * x : ((3 * x) + 3)]) for x in range(5)]))
+
+    cards.append(card)
+    return numbers, cards
 
 
 filename = "./input/4.txt"
@@ -49,24 +59,17 @@ with open(filename) as file:
     lines = file.readlines()
     lines = [line.rstrip() for line in lines]
 
-numbers = []
-cards = []
-card = []
-for i, line in enumerate(lines):
-    if i == 0:
-        numbers = [int(x) for x in line.split(",")]
-        continue
+numbers, cards = generate_cards(lines)
 
-    if line == "":
-        if i > 1:
-            cards.append(card)
-            card = []
-        continue
-
-    card.append(np.array([int(line[3 * x : ((3 * x) + 3)]) for x in range(5)]))
-
-cards.append(card)
 card, mask, number = play_bingo(cards, numbers)
+
+print(
+    """
+##########
+PART 1
+##########
+"""
+)
 
 print(card)
 print(mask)
@@ -78,10 +81,18 @@ print("Winning number: ", sum * number)
 
 card, mask, number = play_bingo(cards, numbers, True)
 
+print(
+    """
+##########
+PART 2
+##########
+"""
+)
+
 print(card)
 print(mask)
 print(number)
 
-sum = get_sum(card, mask, 0)
+sum = get_sum(card, mask)
 print("Sum unmarked: ", sum)
 print("Winning number: ", sum * number)
